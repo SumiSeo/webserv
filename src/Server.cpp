299c0d85658e44c174f2d6056a8d0b8e3c6e0830
Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-12-03 15:47:42 +0100

    feat: read file and create tokens

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,4 +1,20 @@
+#include <fstream>
+#include <sstream>
+
 #include "Server.hpp"
+
+using std::ifstream;
+using std::string;
+using std::string;
+using std::stringstream;
+using std::vector;
+
+typedef vector<string>	t_vecString;
+
+namespace Utils
+{
+	t_vecString	split(const string &str, char delim);
+};
 
 Server::Server() { }
 
@@ -23,8 +39,46 @@ Server	&Server::operator=(const Server &rhs)
 
 Server::t_vecString	Server::readFile(const char filename[])
 {
+	t_vecString		listTokens;
+	ifstream		infile(filename);
+
+	infile.exceptions(ifstream::badbit);
+	for (string content; std::getline(infile, content); )
+	{
+		stringstream	stream(content);
+		while (stream >> content)
+		{
+			if (content[0] == '#') break;
+
+			t_vecString	tokens(Utils::split(content, ';'));
+			for (t_vecString::const_iterator it(tokens.begin()); it != tokens.end(); ++it)
+				listTokens.push_back(*it);
+		}
+	}
+	return listTokens;
 }
 
 void	Server::parseTokens(const t_vecString &tokens)
 {
+}
+
+// --- Static functions ---
+
+t_vecString	Utils::split(const string &str, char delim)
+{
+	t_vecString	words;
+	std::size_t	pos(0);
+
+	while (true)
+	{
+		std::size_t	end = str.find(delim, pos);
+
+		if (end == string::npos) break;
+
+		words.push_back(str.substr(pos, end - pos));
+		words.push_back(str.substr(end, 1)); // add the delim
+		pos = end + 1;
+	}
+	if (pos < str.size()) words.push_back(str.substr(pos));
+	return words;
 }
