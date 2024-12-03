10bee9119705c08346e71e63c83180d35f129179
Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-12-03 15:17:12 +0100

    chore: prepare files for parsing branch

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,1 +1,30 @@
 #include "Server.hpp"
+
+Server::Server() { }
+
+Server::Server(const Server &src) { }
+
+Server::Server(const char fileName[])
+{
+	t_vecString	listTokens(readFile(fileName));
+
+	parseTokens(listTokens);
+}
+
+Server::~Server() { }
+
+Server	&Server::operator=(const Server &rhs)
+{
+
+	return *this;
+}
+
+// --- Private methods ---
+
+Server::t_vecString	Server::readFile(const char filename[])
+{
+}
+
+void	Server::parseTokens(const t_vecString &tokens)
+{
+}
