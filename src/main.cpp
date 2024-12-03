6db260aa297d3cfcda122edc3e35ca98aded84d0
Sumi Seo <sumseo@paul-f4Br5s3.clusters.42paris.fr>
2024-12-03 13:18:51 +0100

    docs/add-readme-and-config

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 16:00:23 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/12/02 19:43:15 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/12/03 12:23:01 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
 
 int	main(int argc, char **argv)
@@ -21,9 +21,8 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 	{
 		cerr << "Usage: " << argv[0] << " <configuration file>" << endl;
-		return 1;
+		return (1);
 	}
-
 	cout << "Initial Release" << endl;
-	return 0;
+	return (0);
 }
