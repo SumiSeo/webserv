85ce3c1e1623b3ab8a5f882d0bddb407245250c8
Sumi Seo <sumseo@paul-f4Br7s6.clusters.42paris.fr>
2024-12-04 12:18:31 +0100

    fix/compile-error

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,85 +1,95 @@
+#include "Server.hpp"
 #include <fstream>
 #include <sstream>
 
-#include "Server.hpp"
-
 using std::ifstream;
-using std::string;
 using std::string;
 using std::stringstream;
 using std::vector;
 
-typedef vector<string>	t_vecString;
+typedef vector<string> t_vecString;
 
 namespace Utils
 {
-	t_vecString	split(const string &str, char delim);
+t_vecString	split(const string &str, char delim);
 };
 
-Server::Server() { }
+Server::Server()
+{
+}
 
-Server::Server(const Server &src) { }
+Server::Server(const Server &src)
+{
+	if (this != &src)
+	{
+		*this = src;
+	}
+}
 
 Server::Server(const char fileName[])
 {
-	t_vecString	listTokens(readFile(fileName));
-
+	t_vecString listTokens(readFile(fileName));
 	parseTokens(listTokens);
 }
 
-Server::~Server() { }
-
-Server	&Server::operator=(const Server &rhs)
+Server::~Server()
 {
+}
 
-	return *this;
+Server &Server::operator=(const Server &rhs)
+{
+	if (this != &rhs)
+	{
+		*this = rhs;
+	}
+	return (*this);
 }
 
 // --- Private methods ---
 
-Server::t_vecString	Server::readFile(const char filename[])
+Server::t_vecString Server::readFile(const char filename[])
 {
-  t_vecString		listTokens;
-	ifstream		infile(filename);
+	t_vecString	listTokens;
 
+	ifstream infile(filename);
 	infile.exceptions(ifstream::badbit);
-	for (string content; std::getline(infile, content); )
+	for (string content; std::getline(infile, content);)
 	{
-		stringstream	stream(content);
+		stringstream stream(content);
 		while (stream >> content)
 		{
-			if (content[0] == '#') break;
-
-			t_vecString	tokens(Utils::split(content, ';'));
+			if (content[0] == '#')
+				break ;
+			t_vecString tokens(Utils::split(content, ';'));
 			for (t_vecString::const_iterator it(tokens.begin()); it != tokens.end(); ++it)
 				listTokens.push_back(*it);
 		}
 	}
-	return listTokens;
+	return (listTokens);
 }
 
-void	Server::parseTokens(const t_vecString &tokens)
+void Server::parseTokens(const t_vecString &tokens)
 {
+	(void)tokens;
 }
 
 // --- Static functions ---
 
-t_vecString	Utils::split(const string &str, char delim)
+t_vecString Utils::split(const string &str, char delim)
 {
 	t_vecString	words;
-	std::size_t	pos(0);
 
+	std::size_t pos(0);
 	while (true)
 	{
-		std::size_t	end = str.find(delim, pos);
-
-		if (end == string::npos) break;
-
+		std::size_t end = str.find(delim, pos);
+		if (end == string::npos)
+			break ;
 		words.push_back(str.substr(pos, end - pos));
 		words.push_back(str.substr(end, 1)); // add the delim
 		pos = end + 1;
 	}
-	if (pos < str.size()) words.push_back(str.substr(pos));
-	return words;
+	if (pos < str.size())
+		words.push_back(str.substr(pos));
+	return (words);
 }
-
