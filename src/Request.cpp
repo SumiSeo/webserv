b5608fa3aa88e2b7387d6313719766fa1273d742
K-Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-12-18 15:32:58 +0100

    feat/retrieve-data

--- src/Request.cpp
+++ src/Request.cpp
@@ -1,4 +1,11 @@
+#include <sys/socket.h>
+
 #include "Request.hpp"
+
+namespace
+{
+	std::size_t const MAX_BUFFER_LENGTH = 8192;
+};
 
 // --- Public --- //
 Request::Request(Request const &src):
@@ -34,6 +41,16 @@ Request &Request::operator=(Request const &rhs)
 
 Request::e_IOReturn Request::retrieve()
 {
+	char buffer[MAX_BUFFER_LENGTH + 1];
+	ssize_t bytesRead = recv(_fd, buffer, MAX_BUFFER_LENGTH, 0);
+
+	if (bytesRead == -1)
+		return IO_ERROR;
+	if (bytesRead == 0)
+		return IO_DISCONNECT;
+
+	buffer[bytesRead] = '\0';
+	_buffer += buffer;
 	return IO_SUCCESS;
 }
 
